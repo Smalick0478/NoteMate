d49a0e5782de724c545d41a04a96ebf2a1ed907b
Smalick0478 <sameermalik0478@gmail.com>
2023-10-23 22:18:22 +0500

    Snackbar and UIDs Added.

--- lib/widgets/notesRelated/custom_app_bar.dart
+++ lib/widgets/notesRelated/custom_app_bar.dart
@@ -1,10 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notemate/models/note.dart';
+import 'package:notemate/providers/note_provider.dart';
+import 'package:notemate/resources/auth_methods.dart';
+import 'package:notemate/resources/firestore_methods.dart';
+import 'package:notemate/utils/colors.dart';
+import 'package:notemate/widgets/reusedComponents/snackbar.dart';
+
+final firestoreService = FirestoreService();
+final userUid = AuthService().useUid;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 class CustomAppBar extends StatelessWidget {
   final VoidCallback onPressed;
+  final noteuid;
+  final folderNoteuid;
 
   const CustomAppBar({
     required this.onPressed,
+    this.noteuid,
+    this.folderNoteuid,
     Key? key,
   }) : super(key: key);
 
@@ -30,6 +46,33 @@ class CustomAppBar extends StatelessWidget {
               child: const Icon(Icons.arrow_back_ios_new_outlined)),
         ),
         Expanded(child: SizedBox()),
+        Container(
+          margin: const EdgeInsets.only(right: 20, top: 30, bottom: 10),
+          height: 50,
+          width: 80,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+          child: ElevatedButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 243, 79, 64))),
+              onPressed: () async {
+                await firestoreService.deleteNote(
+                    uid: noteuid, folder: folderNoteuid);
+
+                Navigator.of(context).pop();
+                CustomSnackBarColored.show(
+                  context,
+                  'Deleted',
+                  'Note Deleted Successfully',
+                  const Duration(seconds: 3),
+                  CustomTheme.dangerColor, // Specify the background color
+                );
+              },
+              child: const Text('Delete')),
+        ),
         Container(
           margin: const EdgeInsets.only(right: 20, top: 30, bottom: 10),
           height: 50,
