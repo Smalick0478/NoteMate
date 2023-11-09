bbea2e7d16fa34f8ffcaada7988a770ec79ef1b6
Smalick0478 <sameermalik0478@gmail.com>
2023-11-10 01:08:28 +0500

    updated.

--- lib/providers/note_provider.dart
+++ lib/providers/note_provider.dart
@@ -1,8 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:notemate/resources/auth_methods.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notemate/screens/functionalities/home_page.dart';
 
 final notesProvider = StreamProvider(((ref) => FirebaseFirestore.instance
     .collection(AuthService().useUid)
     .orderBy('date', descending: true)
-    .snapshots()));
+    .snapshots()));// Define a StateNotifier for searchQuery
+
+// final notesProvider =
+//     StreamProvider<QuerySnapshot<Map<String, dynamic>>>((ref) {
+//   final query = ref.watch(searchQueryProvider);
+//   return FirebaseFirestore.instance
+//       .collection(AuthService().useUid)
+//       .orderBy('date', descending: true)
+//       .where('title', isGreaterThanOrEqualTo: query)
+//       .snapshots();
+// });
