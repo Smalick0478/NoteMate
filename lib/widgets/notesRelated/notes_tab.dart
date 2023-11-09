00d476244bd5db2fd78698b0702d2a78df0bc693
Smalick0478 <sameermalik0478@gmail.com>
2023-11-10 01:11:22 +0500

    updated.

--- lib/widgets/notesRelated/notes_tab.dart
+++ lib/widgets/notesRelated/notes_tab.dart
@@ -4,6 +4,7 @@ import 'package:notemate/widgets/notesRelated/note_cards.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notemate/widgets/notesRelated/notes_search.dart';
 
 class NotesTab extends StatefulWidget {
   const NotesTab({
@@ -27,3 +28,29 @@ class _NotesTabState extends State<NotesTab> {
         ));
   }
 }
+
+// class NotesSearchTab extends StatefulWidget {
+//   const NotesSearchTab({super.key});
+
+//   @override
+//   State<NotesSearchTab> createState() => _NotesSearchTabState();
+// }
+
+// class _NotesSearchTabState extends State<NotesSearchTab> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 20),
+//       child: Scaffold(
+//         body: Consumer(
+//           builder: (context, ref, child) {
+//             final searchQuery = ref.watch(searchQueryProvider).state;
+//             final streamProv = ref.watch(notesSearchProvider(searchQuery));
+//             return NoteSearch(streamProv);
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
